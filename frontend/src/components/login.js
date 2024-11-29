a0e3375eb0fe85f55e7a1dd57413b96c02d9ecc6
Suraj Kumar <surajkumar.ayanokoji.7@gmail.com>
2024-11-29 18:26:53 +0530

    Done minor changes

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -78,7 +78,7 @@ const Login = () => {
             }
         }
     };    
-
+    
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-100">
             <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
