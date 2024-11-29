fae830f7ba4e110efbe56b43b74da104435c44d0
Suraj Kumar <surajkumar.ayanokoji.7@gmail.com>
2024-11-29 17:22:22 +0530

    Added Frontend URL

--- frontend/src/components/axiosInstance.js
+++ frontend/src/components/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.BACKEND_URL,
+    baseURL: process.env.REACT_APP_BACKENDURL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
